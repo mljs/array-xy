d718c2ff8b53a3c1dae5e0d2b87d32ce68552aa8
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-07-25 13:14:50 +0200

    test(maxMerge): case when maximum y is not in x

--- packages/array-xy-max-merge/src/__tests__/test.js
+++ packages/array-xy-max-merge/src/__tests__/test.js
@@ -4,18 +4,18 @@ expect.extend({toBeDeepCloseTo});
 
 const points = {
     x: [100.001, 100.002, 200.01, 200.02, 300.0001, 300.0002],
-    y: [10, 11, 20, 21, 30, 31]
+    y: [10, 11, 20, 21, 32, 31]
 };
 describe('test maxMerge', () => {
     it('default value', () => {
         const merged = maxMerge(points);
-        expect(merged.x).toBeDeepCloseTo([100.002, 200.01, 200.02, 300.0002], 4);
-        expect(merged.y).toEqual([21, 20, 21, 61]);
+        expect(merged.x).toBeDeepCloseTo([100.002, 200.01, 200.02, 300.0001], 4);
+        expect(merged.y).toEqual([21, 20, 21, 63]);
     });
 
     it('custom value', () => {
         const merged = maxMerge(points, {groupWidth: 0.010001});
-        expect(merged.x).toBeDeepCloseTo([100.002, 200.02, 300.0002], 4);
-        expect(merged.y).toEqual([21, 41, 61]);
+        expect(merged.x).toBeDeepCloseTo([100.002, 200.02, 300.0001], 4);
+        expect(merged.y).toEqual([21, 41, 63]);
     });
 });
