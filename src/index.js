b6a8bfeedbd62728fbe39c41f3d0bf0701f88021
Luc Patiny <luc@patiny.com>
2018-06-23 10:27:01 +0200

    work in progress for lactame build

---
+++ src/index.js
@@ -0,0 +1,16 @@
+
+import centroidsMerge from 'packages/array-xy-centroids-merge/src';
+import closestX from 'packages/array-xy-closest-x/src';
+import maxMerge from 'packages/array-xy-max-merge/src';
+import maxY from 'packages/array-xy-max-y/src';
+import sortX from 'packages/array-xy-sort-x/src';
+import weightedMerge from 'packages/array-xy-weighted-merge/src';
+
+export default {
+    centroidsMerge,
+    closestX,
+    maxMerge,
+    maxY,
+    sortX,
+    weightedMerge
+};
