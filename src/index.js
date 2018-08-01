65d8a52457b5b27361dec7049bfc6cf1762bc931
Michaël Zasso <targos@protonmail.com>
2018-08-01 13:57:35 +0200

    add build script

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 export {
   default as centroidsMerge
-} from './packages/array-xy-centroids-merge/src';
-export { default as closestX } from './packages/array-xy-closest-x/src';
-export { default as maxMerge } from './packages/array-xy-max-merge/src';
-export { default as maxY } from './packages/array-xy-max-y/src';
-export { default as sortX } from './packages/array-xy-sort-x/src';
+} from '../packages/array-xy-centroids-merge/src';
+export { default as closestX } from '../packages/array-xy-closest-x/src';
+export { default as maxMerge } from '../packages/array-xy-max-merge/src';
+export { default as maxY } from '../packages/array-xy-max-y/src';
+export { default as sortX } from '../packages/array-xy-sort-x/src';
 export {
   default as weightedMerge
-} from './packages/array-xy-weighted-merge/src';
+} from '../packages/array-xy-weighted-merge/src';
