b6413f128e737f07443ef303f935df5c56e349f2
Luc Patiny <luc@patiny.com>
2021-10-14 20:16:08 +0200

    feat: publish ml-array-xy

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,6 @@ export default {
   input: 'src/index.js',
   output: {
     format: 'cjs',
-    file: 'lib/index.js'
-  }
+    file: 'lib/index.js',
+  },
 };
--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-export {
-  default as centroidsMerge
-} from '../packages/ml-array-xy-centroids-merge/src';
-export { default as closestX } from '../packages/ml-arrayxy-closestx/src';
-export { default as maxMerge } from '../packages/ml-array-xy-max-merge/src';
-export { default as maxY } from '../packages/ml-array-xy-max-y/src';
-export { default as sortX } from '../packages/ml-array-xy-sort-x/src';
-export { default as uniqueX } from '../packages/ml-arrayxy-uniquex/src';
-export { default as filterX } from '../packages/ml-array-xy-filter-x/src';
-export {
-  default as equallySpaced
-} from '../packages/ml-array-xy-equally-spaced/src';
-export {
-  default as weightedMerge
-} from '../packages/ml-array-xy-weighted-merge/src';
-export { default as covariance } from '../packages/ml-array-xy-covariance/src';
+export { default as centroidsMerge } from '../packages/ml-array-xy-centroids-merge/src/index';
+export { default as closestX } from '../packages/ml-arrayxy-closestx/src/index';
+export { default as maxMerge } from '../packages/ml-array-xy-max-merge/src/index';
+export { default as maxY } from '../packages/ml-array-xy-max-y/src/index';
+export { default as sortX } from '../packages/ml-array-xy-sort-x/src/index';
+export { default as uniqueX } from '../packages/ml-arrayxy-uniquex/src/index';
+export { default as filterX } from '../packages/ml-array-xy-filter-x/src/index';
+export { default as equallySpaced } from '../packages/ml-array-xy-equally-spaced/src/index';
+export { default as weightedMerge } from '../packages/ml-array-xy-weighted-merge/src/index';
+export { default as covariance } from '../packages/ml-array-xy-covariance/src/index';
